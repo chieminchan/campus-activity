33ef9355f1bebf6cf2bb766ee0127f74697481ff
chieminchan <chieminchan@qq.com>
2019-04-14 23:46:25 +0800

    [fix] remove database config

--- back-end/config/defaultConfig.js
+++ back-end/config/defaultConfig.js
@@ -3,7 +3,7 @@ module.exports = {
         HOST: '127.0.0.1',
         PORT: 3306,
         USER: 'root',
-        PASSWORD: '123QWEasd',
-        DATABASE:'campus',
+        PASSWORD: '',
+        DATABASE:'',
     }
 };
